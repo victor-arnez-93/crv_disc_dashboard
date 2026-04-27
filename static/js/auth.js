5dd3305016158f9f64ed45b95b431a0100dededd
Victor Arnez <136041084+victor-arnez-93@users.noreply.github.com>
2026-04-27 18:31:34 -0300

    update

--- static/js/auth.js
+++ static/js/auth.js
@@ -109,7 +109,7 @@
                     ${isAdmin ? `
                     <button class="user-modal-btn" id="btnModalConfig">
                         <i class="fas fa-cog"></i> Configuracoes
-                    </button>
+                    </button>` : ""}
                     <button class="user-modal-btn user-modal-btn-sair" id="btnModalSair">
                         <i class="fas fa-sign-out-alt"></i> Sair
                     </button>
